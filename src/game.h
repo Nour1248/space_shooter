6f48046127d53226c2831273d9a9b84e33df0742
= <=>
2023-07-26 16:06:29 +0000

    --

--- src/game.h
+++ src/game.h
@@ -1,15 +1,16 @@
 #ifndef _GAME_H
 #define _GAME_H 69
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+// #ifdef __cplusplus
+// extern "C"
+// {
+// #endif
 
-#include "structs.h"
 #include <SDL2/SDL.h>
 #include <pthread.h>
 #include <stdbool.h>
+
+#include "structs.h"
 
 #define FPS 60
 #define DELTA 1000 / FPS // AKA FRAME DELAY
@@ -17,83 +18,89 @@ extern "C"
 
 #define BULLETS_COUNT 50
 #define METEORS_COUNT 50
-#define ANIMATION_UNIT_COUNT 20
+#define ANIMATION_UNIT_COUNT 50
 
 #define SHIP_VELOCITY 20
 #define BULLET_VELOCITY 10
 
-#define DELAY(ft)                                                             \
-  if (ft < DELTA)                                                             \
-    {                                                                         \
-      SDL_Delay (DELTA - ft);                                                 \
+#define DELAY(ft)                                                                                                      \
+    if (ft < DELTA)                                                                                                    \
+    {                                                                                                                  \
+        SDL_Delay(DELTA - ft);                                                                                         \
     }
 
-  // global objs/vars
-  extern int32_t g_WIDTH;
+// global objs/vars
+extern int32_t g_WIDTH;
 
-  extern int32_t g_HEIGHT;
+extern int32_t g_HEIGHT;
 
-  extern uint64_t g_clock;
+extern uint64_t g_clock;
 
-  extern App g_app;
+extern App g_app;
 
-  extern Entity g_player;
+extern Entity g_player;
 
-  extern Entity *g_bullets[BULLETS_COUNT];
+extern Entity *g_bullets[BULLETS_COUNT];
 
-  extern Entity *g_meteors[METEORS_COUNT];
+extern Entity *g_meteors[METEORS_COUNT];
 
-  extern char *g_explosion[9];
+extern Animation *g_animationUnits[ANIMATION_UNIT_COUNT];
 
-  // init protos
-  extern void init_sdl (void);
+extern char *g_explosionSheet[9];
 
-  // input protos
-  extern void handle_input (void);
+// init protos
+extern void init_sdl(void);
 
-  extern void handle_keydown (SDL_KeyboardEvent *event);
+// input protos
+extern void handle_input(void);
 
-  extern void handle_keyup (SDL_KeyboardEvent *event);
+extern void handle_keydown(SDL_KeyboardEvent *event);
 
-  // draw protos
-  extern void clean_up (void);
+extern void handle_keyup(SDL_KeyboardEvent *event);
 
-  extern void clear (void);
+// draw protos
+extern void clean_up(void);
 
-  extern void render_scene (void);
+extern void clear(void);
 
-  extern SDL_Texture *load_texture (char *filename);
+extern void render_scene(void);
 
-  extern void blit (Entity *entity);
+extern SDL_Texture *load_texture(char *filename);
 
-  extern void blit_scaled (Entity *entity, int32_t w, int32_t h);
+extern void blit(Entity *entity);
 
-  extern void blit_scaledAnimation (Animation *a, SDL_Rect *r);
+extern void blit_scaled(Entity *entity, int32_t w, int32_t h);
 
-  extern bool is_outOfBounds (Entity *e);
+extern void blit_scaledAnimation(Animation *a, SDL_Rect *r);
 
-  extern bool are_colliding (SDL_Rect *r1, SDL_Rect *r2);
+extern bool is_outOfBounds(Entity *e);
 
-  extern void animate_player (Entity *entity);
+extern bool are_colliding(SDL_Rect *r1, SDL_Rect *r2);
 
-  extern void init_bullets (void);
+extern void animate_player(Entity *entity);
 
-  extern void blit_bullets (Entity **bulls);
+extern void init_bullets(void);
 
-  extern void shoot (Entity **bulls, Entity *player);
+extern void blit_bullets(Entity **bulls);
 
-  extern void animate_bullets (Entity **bulls);
+extern void shoot(Entity **bulls, Entity *player);
 
-  extern void init_meteors (void);
+extern void animate_bullets(Entity **bulls);
 
-  extern void blit_meteors (Entity **meteors);
+extern void init_meteors(void);
 
-  extern void animate_meteors (void);
+extern void blit_meteors(Entity **meteors);
 
-  extern void animation (Animation *a, char **sheet, size_t sheet_size);
+extern void init_animationUnits(void);
 
-#ifdef __cplusplus
-}
-#endif
+extern void animation(Animation *a, char **sheet, size_t sheet_size);
+
+extern void play_explosionAnimation(void);
+
+extern void animate_meteors(void);
+
+// #ifdef __cplusplus
+// }
+// #endif
 
 #endif //_GAME_H
--- src/main.c
+++ src/main.c
@@ -1,76 +1,83 @@
-#include "game.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdlib.h>
 
+#include "game.h"
+
 Entity g_player;
 uint64_t g_clock;
 
-int
-main ()
+int main()
 {
-  //
-  init_sdl ();
-  init_bullets ();
-  init_meteors ();
+    //
+    init_sdl();
+    init_bullets();
+    init_meteors();
+    init_animationUnits();
 
-  atexit (&clean_up);
+    atexit(&clean_up);
 
-  // background
-  Entity background;
-  background.texture = load_texture ("assets/starfield.png");
-  background.rect.x = 0;
-  background.rect.y = 0;
+    // background
+    Entity background;
+    background.texture = load_texture("assets/starfield.png");
+    background.rect.x = 0;
+    background.rect.y = 0;
 
-  // player ship
-  g_player.texture = load_texture ("assets/playerShip1_orange.png");
-  g_player.rect.x = (g_WIDTH / 2) - (99 / 2); // centering the ship
-  g_player.rect.y = (g_HEIGHT / 2) - (75 / 2);
+    // player ship
+    g_player.texture = load_texture("assets/playerShip1_orange.png");
+    g_player.rect.x = (g_WIDTH / 2) - (99 / 2); // centering the ship
+    g_player.rect.y = (g_HEIGHT / 2) - (75 / 2);
 
-  uint64_t frame_time;
+    uint64_t frame_time;
 
-  Animation *a = malloc (sizeof (Animation));
-  a->texture = NULL;
-  a->start = 0;
-  a->delay = 1000;
-  a->idx = 0;
-  a->rect.x = 200;
-  a->rect.y = 200;
-  a->rect.w = 200;
-  a->rect.h = 200;
+    // uint64_t idx = 1;
 
-  while (69)
+    // double fps;
+
+    while (69)
     {
-      g_clock = SDL_GetTicks64 (); // FBS CTRL
+        g_clock = SDL_GetTicks64(); // FBS CTRL
 
-      clear ();
+        clear();
 
-      SDL_GetWindowSize (g_app.window, &g_WIDTH, &g_HEIGHT);
+        SDL_GetWindowSize(g_app.window, &g_WIDTH, &g_HEIGHT);
 
-      handle_input ();
+        handle_input();
 
-      blit_scaled (&background, g_WIDTH, g_HEIGHT);
+        blit_scaled(&background, g_WIDTH, g_HEIGHT);
 
-      blit (&g_player);
+        blit(&g_player);
 
-      blit_bullets (g_bullets);
+        blit_bullets(g_bullets);
 
-      shoot (g_bullets, &g_player);
+        shoot(g_bullets, &g_player);
 
-      blit_meteors (g_meteors);
+        blit_meteors(g_meteors);
 
-      animate_player (&g_player);
+        animate_player(&g_player);
 
-      animate_bullets (g_bullets);
+        animate_bullets(g_bullets);
 
-      animate_meteors ();
+        animate_meteors();
 
-      animation (a, g_explosion, 9);
+        // for (size_t i = 0; i < ANIMATION_UNIT_COUNT; i++)
+        // {
+        //     animation(g_animationUnits[i], g_explosionSheet, 9);
+        // }
 
-      render_scene ();
+        play_explosionAnimation();
 
-      frame_time = SDL_GetTicks () - g_clock; // FBS CTRL
-      DELAY (frame_time)                      // FBS CTRL
+        render_scene();
+
+        // fps = (double)idx / (g_clock / 1000);
+        // if (fps != INFINITY)
+        // {
+        //     printf("%f\n", fps);
+        // }
+        // idx++;
+
+        frame_time = SDL_GetTicks64() - g_clock; // FBS CTRL
+        DELAY(frame_time)                        // FBS CTRL
     }
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
